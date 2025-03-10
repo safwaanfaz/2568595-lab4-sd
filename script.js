7c5c4862c748be455dc96fe16b797f01b286bec5
Safwaanfaz <2568595@students.wits.ac.za>
2025-03-10 17:48:36 +0200

    first commit

--- script.js
+++ script.js
@@ -3,49 +3,51 @@ const submitBtn = document.getElementById('submit-btn');
 const countryInfoSection = document.getElementById('country-info');
 const borderingCountriesSection = document.getElementById('bordering-countries');
 
-submitBtn.addEventListener('click', fetchCountryData);
-
-function fetchCountryData() {
-    const countryName = countryInput.value.trim();
-
+submitBtn.addEventListener('click', () => {
+    const countryName = countryInput.value;
     countryInfoSection.innerHTML = '<h2>Country Information</h2>';
     borderingCountriesSection.innerHTML = '<h2>Bordering Countries</h2>';
 
     fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Country not found');
-            }
-            return response.json();
-        })
+        .then(response => response.json())
         .then(data => {
             const country = data[0];
 
-            countryInfoSection.innerHTML += `
-                <img src="${country.flags.png}" alt="Flag of ${country.name.common}" width="100">
-                <p>Capital: ${country.capital ? country.capital[0] : 'N/A'}</p>
-                <p>Population: ${country.population.toLocaleString()}</p>
-                <p>Region: ${country.region}</p>
+            countryInfoSection.innerHTML = `
+                <h2>${country.name.common}</h2>
+                <img src="${country.flags.png}" width="100"> 
+                <p>Capital: ${country.capital?.[0] || 'N/A'}</p> 
+                <p>Population: ${country.population.toLocaleString()}</p>  
+                <p>Region: ${country.region}</p> 
             `;
 
-            if (country.borders && country.borders.length > 0) {
-                fetch(`https://restcountries.com/v3.1/alpha?codes=${country.borders.join(',')}`)
-                    .then(response => response.json())
-                    .then(borderCountries => {
-                        borderCountries.forEach(borderCountry => {
+            borderingCountriesSection.innerHTML = '<h2>Bordering Countries</h2>';
+
+            if (country.borders) {
+                const borderPromises = country.borders.map(borderCode =>
+                    fetch(`https://restcountries.com/v3.1/alpha/${borderCode}`)
+                        .then(response => response.json())
+                );
+
+                Promise.all(borderPromises)
+                    .then(bordersData => {
+                        bordersData.forEach(border => {
                             borderingCountriesSection.innerHTML += `
                                 <div>
-                                    <img src="${borderCountry.flags.png}" alt="Flag of ${borderCountry.name.common}" width="50">
-                                    <span>${borderCountry.name.common}</span>
+                                    <img src="${border[0].flags.png}" width="50">  
+                                    <span>${border[0].name.common}</span>  
                                 </div>
                             `;
                         });
+                    })
+                    .catch(() => {
+                        borderingCountriesSection.innerHTML += '<p>Error fetching border countries</p>';
                     });
             } else {
                 borderingCountriesSection.innerHTML += '<p>No bordering countries</p>';
             }
         })
-        .catch(error => {
-            countryInfoSection.innerHTML += `<p>Error: ${error.message}</p>`;
+        .catch(() => {
+            countryInfoSection.innerHTML = '<p>Country not found, please try again</p>';
         });
-}
+});
